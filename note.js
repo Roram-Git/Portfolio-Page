eb80d5e7078f82a223b5c32d2c485cb0e796991a
R <rohan@pkm47.com>
2025-03-22 14:55:59 -0700

    test

---
+++ note.js
@@ -0,0 +1,27 @@
+/*
+ control c stops a terminal execution
+ 
+ clone repo
+ go to github
+ press code in portfolio page
+ copy url
+ git clone <url>
+
+ vs code shortcut 
+ "code ." opens a new vs code window
+ 
+ to push repo
+ git add .
+ git commit -m <message>(if multi word use "")
+ git push
+
+  git config user.email "rohan@pkm47.com"
+  git config user.name "R"
+
+
+
+homework:
+  settings:
+  change git to be the main terminal
+  add autosave after delay
+ */
